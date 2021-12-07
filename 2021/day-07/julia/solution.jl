71b67f03e8084c9c2a0b94833d40e619bfb9ae7d
jwnet <53386281+jwnet@users.noreply.github.com>
2021-12-07 08:10:18 -0600

    2021/day-07/julia: part 1 complete

--- 2021/day-07/julia/solution.jl
+++ 2021/day-07/julia/solution.jl
@@ -1,28 +1,24 @@
 using DelimitedFiles
 using Statistics
 
-function solve(data)
-	x = 0.0
-	pos1
-	first = true
+
+function part1(data)
+	median = median(sort(data[1:,]))
+	cost = 0.0
 	for pos in data
-		if first
-			last = pos
-			first = false
-			continue
-		end
-		x = 
+		cost += abs(pos-median)
+	end
+	return Int.(cost)
 end
 
-part1(data) = solve(data)
 # part2(data) = 
 
 sample = readdlm("sample", ',', Int)
-# input = readdlm("input", ',', Int)
+input = readdlm("input", ',', Int)
 
 println("Part 1")
-# println("  sample: ", part1(sample))
-# println("  input : ", part1(input))
+println("  sample: ", part1(sample))
+println("  input : ", part1(input))
 # println("Part 2")
 # println("  sample: ", part2(sample))
 # println("  input : ", part2(input))
