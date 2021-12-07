b2ee55418c634e75b3439426cc4ba1cb23736d5c
jwnet <53386281+jwnet@users.noreply.github.com>
2021-12-07 08:03:51 -0600

    2021/day-07/go: part 1 complete

--- 2021/day-07/go/main.go
+++ 2021/day-07/go/main.go
@@ -3,26 +3,34 @@ package main
 import (
 	"fmt"
 	"math"
+	"sort"
 )
 
 func part1(data []float64) int {
-	//find median then compute cost
+	sort.Float64s(data)
+	middle := float64(len(data)) / 2
+	median := 0.0
+	if len(data) % 2 == 1 {
+		median = data[int(middle)]
+	} else {
+		median = math.Round((data[int(middle) - 1] + data[int(middle)]) / 2)
+	}
+
+	cost := 0
+	for _, pos := range data {
+		cost += int(math.Abs(pos - median))
+	}
+	return cost
 }
 
-func part2(data) int {
-	// sort first!!!
-	x := data[0]
-	for i := 1; i < len(data); i++ {
-		x = (data[i] + x) / 2
-	}
-	pos = int(math.Round(x))
-	//compute cost
-}
+// func part2(data) int {
+
+// }
 
 func main() {
 	fmt.Println("Part One")
 	fmt.Printf("  sample: %d\n", part1(sample))
-	// fmt.Printf("  input : %d\n", part1(input))
+	fmt.Printf("  input : %d\n", part1(input))
 	// fmt.Println("Part Two")
 	// fmt.Printf("  sample: %d\n", part2(sample))
 	// fmt.Printf("  input : %d\n", part2(input))
