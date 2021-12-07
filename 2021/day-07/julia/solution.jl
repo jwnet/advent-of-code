8ee2666ddbc0f33c7e10af72a7cb9320fda36cd8
jw <53386281+jwnet@users.noreply.github.com>
2021-12-07 12:23:10 -0600

    2021/day-07/julia: completed

--- 2021/day-07/julia/solution.jl
+++ 2021/day-07/julia/solution.jl
@@ -1,24 +1,27 @@
 using DelimitedFiles
 using Statistics
 
+cost1(data, pos) = Int.(sum(map(x -> abs(x-pos), data)))
+part1(data) = cost1(data, median(data))
 
-function part1(data)
-	median = median(sort(data[1:,]))
-	cost = 0.0
-	for pos in data
-		cost += abs(pos-median)
+cost2(data, pos) = Int.(sum(map(x -> abs(x-pos)*(abs(x-pos)+1)/2, data)))
+function part2(data)
+	l, r = first(data), last(data)
+	cost = cost2(data, l)
+	for p in (l+1):r
+		c = cost2(data, p)
+		c > cost && break
+		cost = c
 	end
-	return Int.(cost)
+	return cost
 end
 
-# part2(data) = 
-
-sample = readdlm("sample", ',', Int)
-input = readdlm("input", ',', Int)
+sample = sort(readdlm("sample", ',', Int)[1,:])
+input = sort(readdlm("input", ',', Int)[1,:])
 
 println("Part 1")
 println("  sample: ", part1(sample))
 println("  input : ", part1(input))
-# println("Part 2")
-# println("  sample: ", part2(sample))
-# println("  input : ", part2(input))
+println("Part 2")
+println("  sample: ", part2(sample))
+println("  input : ", part2(input))
