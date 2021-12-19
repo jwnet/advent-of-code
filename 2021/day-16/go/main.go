09c822fbfb5196c09ccc4f0cc6b23826d9e0d0b8
jw <53386281+jwnet@users.noreply.github.com>
2021-12-18 20:23:18 -0600

    2021/day-16/go: refactor and "clean" up

--- 2021/day-16/go/main.go
+++ 2021/day-16/go/main.go
@@ -8,6 +8,8 @@ import (
 
 type packetData struct {
 	hexStr string
+	bitBuffer     uint64
+	nBufferedBits uint64
 }
 
 func newPacketData(hex string) *packetData {
@@ -16,21 +18,36 @@ func newPacketData(hex string) *packetData {
 
 func (p *packetData) nextByte() byte {
 	if len(p.hexStr) < 2 {
-		panic("trying to read into something that's not there...")
+		panic("packetData.nextByte: trying to read into something that's not there...")
 	}
 	b, err := strconv.ParseUint(p.hexStr[:2], 16, 64)
 	if err != nil {
-		panic("your damned input is invalid")
+		panic("packetData.nextByte: your damned input is invalid")
 	}
 	p.hexStr = p.hexStr[2:]
 	return byte(b)
 }
 
-func (p *packetData) empty() bool {
-	return len(p.hexStr) == 0
+// NextNBits returns up to 64 bits at a time. Panics if you request
+// more than 64 bits. Behave. We also panic if you run out of packet
+// data to read. Yikes!
+func (p *packetData) NextNBits(n uint64) uint64 {
+	for p.nBufferedBits < n {
+		if p.nBufferedBits+8 > 64 {
+			panic("packetData.NextNBits: you requested more than 64 bits")
+		}
+		p.bitBuffer = (p.bitBuffer << 8) + uint64(p.nextByte())
+		p.nBufferedBits += 8
+	}
+	b := p.bitBuffer >> (p.nBufferedBits - n)
+	adjust := 64 + n - p.nBufferedBits
+	p.bitBuffer = (p.bitBuffer << adjust) >> adjust
+	p.nBufferedBits -= n
+	return b
 }
 
 //---------------------------------------------------------
+
 const (
 	id_sum  = 0
 	id_prod = 1 // product of sub-packets or single packet
@@ -102,15 +119,14 @@ func eq(s []uint64) uint64 {
 }
 
 type packet struct {
-	workingBits        uint64
-	nWorkingBits, used uint64
-	version, id        uint64
-	literal            uint64
-	lengthType         uint64
-	sub                []*packet
+	nBits       uint64
+	version, id uint64
+	literal     uint64
+	lengthType  uint64
+	sub         []*packet
 }
 
-func (p *packet) string(prefix string) string {
+func (p *packet) String(prefix string) string {
 	b := strings.Builder{}
 	b.WriteString(fmt.Sprintf("%s packet:\n", prefix))
 	b.WriteString(fmt.Sprintf("%s version: %d\n", prefix, p.version))
@@ -119,13 +135,16 @@ func (p *packet) string(prefix string) string {
 	if len(p.sub) > 0 {
 		b.WriteString(fmt.Sprintf("%s%d sub-packets: \n", prefix, len(p.sub)))
 		for _, sp := range p.sub {
-			b.WriteString(sp.string(prefix + "\t"))
+			b.WriteString(sp.String(prefix + "\t"))
 		}
 	}
 	return b.String()
 }
 
 func (p *packet) value() (val uint64) {
+	if p.id == id_lit {
+		return p.literal
+	}
 	subVals := make([]uint64, len(p.sub))
 	for i, sp := range p.sub {
 		subVals[i] = sp.value()
@@ -139,8 +158,6 @@ func (p *packet) value() (val uint64) {
 		val = min(subVals)
 	case id_max:
 		val = max(subVals)
-	case id_lit:
-		val = p.literal
 	case id_gt:
 		val = gt(subVals)
 	case id_lt:
@@ -152,79 +169,14 @@ func (p *packet) value() (val uint64) {
 }
 
 func (p *packet) parse(pd *packetData) {
-	p.parseVersion(pd)
-	p.parseId(pd)
+	p.version = p.takeNBits(pd, 3)
+	p.id = p.takeNBits(pd, 3)
 	switch p.id {
 	case id_lit:
 		p.parseLiteral(pd)
 	default:
 		p.parseOperator(pd)
 	}
-}
-
-func (p *packet) parseOperator(pd *packetData) {
-	p.parseLengthType(pd)
-	switch p.lengthType {
-	case len_15:
-		p.parseL15(pd)
-	case len_11:
-		p.parseL11(pd)
-	}
-}
-
-func (p *packet) parseL11(pd *packetData) {
-	npackets := p.takeNBits(pd, 11)
-	var used uint64
-	carryover, ncarryover := p.workingBits, p.nWorkingBits
-	for i := uint64(0); i < npackets; i++ {
-		np := new(packet)
-		np.workingBits = carryover
-		np.nWorkingBits = ncarryover
-
-		np.parse(pd)
-		p.sub = append(p.sub, np)
-
-		used += np.used
-		carryover = np.workingBits
-		ncarryover = np.nWorkingBits
-	}
-	p.workingBits = carryover
-	p.nWorkingBits = ncarryover
-	p.used += used
-}
-
-func (p *packet) parseL15(pd *packetData) {
-	nbits := p.takeNBits(pd, 15)
-	var used uint64
-	carryover, ncarryover := p.workingBits, p.nWorkingBits
-
-	for used < nbits {
-		np := new(packet)
-		np.workingBits = carryover
-		np.nWorkingBits = ncarryover
-
-		np.parse(pd)
-		p.sub = append(p.sub, np)
-
-		used += np.used
-		carryover = np.workingBits
-		ncarryover = np.nWorkingBits
-	}
-	p.workingBits = carryover
-	p.nWorkingBits = ncarryover
-	p.used += used
-}
-
-func (p *packet) parseLengthType(pd *packetData) {
-	p.lengthType = p.takeNBits(pd, 1)
-}
-
-func (p *packet) parseVersion(pd *packetData) {
-	p.version = p.takeNBits(pd, 3)
-}
-
-func (p *packet) parseId(pd *packetData) {
-	p.id = p.takeNBits(pd, 3)
 }
 
 func (p *packet) parseLiteral(pd *packetData) {
@@ -236,6 +188,40 @@ func (p *packet) parseLiteral(pd *packetData) {
 	p.literal = literal
 }
 
+func (p *packet) parseOperator(pd *packetData) {
+	p.lengthType = p.takeNBits(pd, 1)
+	switch p.lengthType {
+	case len_15:
+		p.parseL15(pd)
+	case len_11:
+		p.parseL11(pd)
+	}
+}
+
+func (p *packet) parseL11(pd *packetData) {
+	npackets := p.takeNBits(pd, 11)
+	var used uint64
+	for i := uint64(0); i < npackets; i++ {
+		np := new(packet)
+		np.parse(pd)
+		p.sub = append(p.sub, np)
+		used += np.nBits
+	}
+	p.nBits += used
+}
+
+func (p *packet) parseL15(pd *packetData) {
+	nbits := p.takeNBits(pd, 15)
+	var used uint64
+	for used < nbits {
+		np := new(packet)
+		np.parse(pd)
+		p.sub = append(p.sub, np)
+		used += np.nBits
+	}
+	p.nBits += used
+}
+
 func (p *packet) versionSum() uint64 {
 	v := p.version
 	for _, sp := range p.sub {
@@ -245,22 +231,8 @@ func (p *packet) versionSum() uint64 {
 }
 
 func (p *packet) takeNBits(pd *packetData, n uint64) uint64 {
-	if n <= 0 {
-		panic("negative nelly")
-	}
-	for p.nWorkingBits < uint64(n) {
-		if p.nWorkingBits+8 > 64 {
-			panic("too many bits!!!")
-		}
-		p.workingBits = (p.workingBits << 8) + uint64(pd.nextByte())
-		p.nWorkingBits += 8
-	}
-	b := p.workingBits >> (uint64(p.nWorkingBits) - uint64(n))
-	adjust := 64 + n - p.nWorkingBits
-	p.nWorkingBits -= n
-	p.workingBits = (p.workingBits << adjust) >> adjust
-	p.used += n
-	return b
+	p.nBits += n
+	return pd.NextNBits(n)
 }
 
 func part1(packetBlob string) uint64 {
@@ -279,14 +251,14 @@ func part2(packetBlob string) uint64 {
 
 func main() {
 	fmt.Println("Part One")
-	fmt.Printf("\tsample1: %d\n", part1("D2FE28"))                       // 4
-	fmt.Printf("\tsample2: %d\n", part1("38006F45291200"))               // 9
-	fmt.Printf("\tsample3: %d\n", part1("EE00D40C823060"))               // 14
-	fmt.Printf("\tsample4: %d\n", part1("8A004A801A8002F478"))           // 16
-	fmt.Printf("\tsample5: %d\n", part1("620080001611562C8802118E34"))   // 12
-	fmt.Printf("\tsample6: %d\n", part1("C0015000016115A2E0802F182340")) // 23
-	fmt.Printf("\tsample7: %d\n", part1("A0016C880162017C3686B18A3D4780"))
-	fmt.Printf("\t  input: %d\n", part1(input)) // 951
+	fmt.Printf("\tsample1: %d\n", part1("D2FE28"))                         // 4
+	fmt.Printf("\tsample2: %d\n", part1("38006F45291200"))                 // 9
+	fmt.Printf("\tsample3: %d\n", part1("EE00D40C823060"))                 // 14
+	fmt.Printf("\tsample4: %d\n", part1("8A004A801A8002F478"))             // 16
+	fmt.Printf("\tsample5: %d\n", part1("620080001611562C8802118E34"))     // 12
+	fmt.Printf("\tsample6: %d\n", part1("C0015000016115A2E0802F182340"))   // 23
+	fmt.Printf("\tsample7: %d\n", part1("A0016C880162017C3686B18A3D4780")) // 31
+	fmt.Printf("\t  input: %d\n", part1(input))                            // 951
 	fmt.Println("Part 2")
 	fmt.Printf("\t sample8: %d\n", part2("C200B40A82"))                 // 3
 	fmt.Printf("\t sample9: %d\n", part2("04005AC33890"))               // 54
