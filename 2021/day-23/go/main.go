e2b9d574b69e15b6e11f67b3741dfd9d08d943bb
jw <53386281+jwnet@users.noreply.github.com>
2022-01-03 15:43:36 -0600

    2021/day-23/go complete

--- 2021/day-23/go/main.go
+++ 2021/day-23/go/main.go
@@ -5,114 +5,72 @@ import (
 	"math"
 )
 
-const (
-	A = iota
-	B
-	C
-	D
-	Empty
-)
-
-const (
-	RA = 2
-	RB = 4
-	RC = 6
-	RD = 8
-)
-
-var rooms = map[int]int{A: RA, B: RB, C: RC, D: RD}
-var atype = map[rune]int{'A':A, 'B':B, 'C':C, 'D':D, 0:Empty}
-var cost = map[int]int{A: 1, B: 10, C: 100, D: 1000}
+var hallwayRoomIdxs = [4]int{2, 4, 6, 8}
+var roomIdx = map[rune]int{'A': 0, 'B': 1, 'C': 2, 'D': 3}
+var cost = map[rune]int{'A': 1, 'B': 10, 'C': 100, 'D': 1000}
 var costs []int
 
 type burrow struct {
 	hall  [11]rune
-	rooms [4][2]rune
+	rooms [4][]rune
 	cost  int
 }
 
-func (b burrow) String() string {
-	s := ""
-	for _, r := range b.hall {
-		if r == 0 {
-			s += " "
-		} else {
-			s += string(r)
-		}
-	}
-	s += "\n"
-	r1, r2 := "##", "##"
-	for _, r := range b.rooms {
-		if r[0] != 0 {
-			r1 = r1 + string(r[0])
-		} else {
-			r1 = r1 + " "
-		}
-		if r[1] != 0 {
-			r2 = r2 + string(r[1])
-		} else {
-			r2 = r2 + " "
-		}
-		r1 = r1 + "#"
-		r2 = r2 + "#"
-	}
-	s += r1 + "#\n" + r2  + "#"
-	return s
-}
-
 func (b burrow) done() bool {
-	return b.rooms[0][0] == 'A' && b.rooms[0][1] == 'A' &&
-		b.rooms[1][0] == 'B' && b.rooms[1][1] == 'B' &&
-		b.rooms[2][0] == 'C' && b.rooms[2][1] == 'C' &&
-		b.rooms[3][0] == 'D' && b.rooms[3][1] == 'D'
+	for i, room := range b.rooms {
+		for _, o := range room {
+			if int(o-'A') != i {
+				return false
+			}
+		}
+	}
+	return true
 }
 
-var hallSpots = [7]int{0,1,3,5,7,9,10}
+func (b *burrow) clone() burrow {
+	nb := burrow{}
+	nb.hall = b.hall
+	nb.cost = b.cost
+	for i, room := range b.rooms {
+		nr := make([]rune, len(room))
+		copy(nr, room)
+		nb.rooms[i] = nr
+	}
+	return nb
+}
+
+// an array of all hallway indexs we can move to
+var hallIdxs = [7]int{0, 1, 3, 5, 7, 9, 10}
+
 func arrange(b burrow) {
 	if b.done() {
 		costs = append(costs, b.cost)
 		return
 	}
-	// make every possible move
 	for i, r := range b.hall {
-		a := atype[r]
-		if a != Empty {
-			if nb, ok := b.hallToRoom(i); ok {
-				arrange(nb)
-			}
-		}
-	}
-	for i, room := range b.rooms {
-		A1, A2 := room[0], room[1]
-		a1, _ := atype[A1]
-		a2, _ := atype[A2]
-		if (a1 == Empty && a2 == Empty) || (a1 == i && a2 == i)  {
+		if r == 0 {
 			continue
 		}
-		switch {
-		case a1 == i && a2 != i:
-			for _, hallIdx := range hallSpots {
-				if nb, ok := b.roomToHall(i,0,hallIdx); ok {
+		if nb, ok := b.hallToRoom(i); ok {
+			arrange(nb)
+		}
+	}
+	for ri, room := range b.rooms {
+		for i, occupent := range room {
+			if occupent == 0 {
+				continue
+			}
+			if !b.canExitRoom(ri, i) {
+				break
+			}
+			if int(occupent-'A') != ri {
+				if nb, ok := b.roomToRoom(ri, i); ok {
 					arrange(nb)
+					break
 				}
 			}
-		case a1 != Empty && a1 != i:
-			if nb, ok := b.roomToRoom(i,0); ok {
-				arrange(nb)
-				continue // because there is no cheaper cost
-			}
-			for _, hallIdx := range hallSpots {
-				if nb, ok := b.roomToHall(i,0,hallIdx); ok {
-					arrange(nb)
-				}
-			}
-		case a1 == Empty && a2 != i:
-			if nb, ok := b.roomToRoom(i,1); ok {
-				arrange(nb)
-				continue // because there is no cheaper cost
-			}
-			for _, hallIdx := range hallSpots {
-				if nb, ok := b.roomToHall(i,1,hallIdx); ok {
+			for _, hi := range hallIdxs {
+				if nb, ok := b.roomToHall(ri, i, hi); ok {
 					arrange(nb)
 				}
 			}
@@ -120,83 +78,104 @@ func arrange(b burrow) {
 	}
 }
 
-func (b burrow) roomToHall(r, idx, hallIdx int) (nb burrow, ok bool) {
-	A := b.rooms[r][idx]
-	a := atype[A]
-	cri := rooms[r]
-	if !b.hallClear(cri, hallIdx) {
-		return
-	}
-	b.rooms[r][idx] = 0
-	b.hall[hallIdx] = A
-	b.cost += (abs(cri-hallIdx) + 1 + idx) *  cost[a]
-	ok = true
-	nb = b
-	return
-}
-
-func (b burrow) roomToRoom(r, idx int) (nb burrow, ok bool) {
-	A := b.rooms[r][idx]
-	a := atype[A]
-	cri := rooms[r]
-	ri := rooms[a]
-	if !b.hallClear(cri, ri) {
-		return
-	}
-	if b.rooms[a][0] == 0 {
-		switch {
-		case b.rooms[a][1] == 0:
-			b.rooms[r][idx] = 0
-			b.rooms[a][1] = A
-			b.cost += (abs(cri - ri) + 3 + idx) * cost[a]
-			ok = true
-		case b.rooms[a][1] == A:
-			b.rooms[r][idx] = 0
-			b.rooms[a][0] = A
-			b.cost += (abs(cri - ri) + 2 + idx) * cost[a]
-			ok = true
+// roomOpen returns whether we can move into a room, and the index
+// that we can move into.
+func (b *burrow) roomOpen(ri int) (idx int, ok bool) {
+	nlen := len(b.rooms[ri])
+	for i := nlen - 1; i >= 0; i-- {
+		switch b.rooms[ri][i] {
+		case rune('A' + ri):
+			continue
+		case 0:
+			return i, true
+		default:
+			return -1, false
 		}
 	}
-	nb = b
-	return
+	return -1, false
 }
 
-func (b burrow) hallToRoom(idx int) (nb burrow, ok bool) {
-	A := b.hall[idx]
-	a := atype[A]
-	ri, rok := rooms[a]
-	if !rok || !b.hallClear(idx, ri) {
-		return
+func (b *burrow) canExitRoom(ri, idx int) bool {
+	if b.rooms[ri][idx] == 0 {
+		return false
 	}
-	if b.rooms[a][0] == 0 {
-		switch {
-		case b.rooms[a][1] == 0:
-			b.hall[idx] = 0
-			b.rooms[a][1] = A
-			b.cost += (abs(idx - ri) + 2) * cost[a]
-			ok = true
-		case b.rooms[a][1] == A:
-			b.hall[idx] = 0
-			b.rooms[a][0] = A
-			b.cost += (abs(idx - ri) + 1) * cost[a]
-			ok = true
+	for i := idx - 1; i >= 0; i-- {
+		if b.rooms[ri][i] != 0 {
+			return false
 		}
 	}
-	nb = b
-	return
+	if int(b.rooms[ri][idx]-'A') != ri {
+		return true
+	}
+	for i := idx + 1; i < len(b.rooms[ri]); i++ {
+		if int(b.rooms[ri][i]-'A') != ri {
+			return true
+		}
+	}
+	return false
+}
+
+func (b *burrow) roomToHall(r, idx, hallIdx int) (nb burrow, ok bool) {
+	occupent := b.rooms[r][idx]
+	src_hri := hallwayRoomIdxs[r]
+	if !b.hallClear(src_hri, hallIdx) {
+		return
+	}
+	nb = b.clone()
+	nb.rooms[r][idx] = 0
+	nb.hall[hallIdx] = occupent
+	nb.cost += (abs(src_hri-hallIdx) + 1 + idx) * cost[occupent]
+	return nb, true
+}
+
+func (b *burrow) roomToRoom(r, idx int) (nb burrow, ok bool) {
+	occupent := b.rooms[r][idx]
+	dst_ri := roomIdx[occupent]
+	src_hri := hallwayRoomIdxs[r]
+	dst_hri := hallwayRoomIdxs[dst_ri]
+	if !b.hallClear(src_hri, dst_hri) {
+		return
+	}
+	oidx, open := b.roomOpen(dst_ri)
+	if !open {
+		return
+	}
+	nb = b.clone()
+	nb.rooms[r][idx] = 0
+	nb.rooms[dst_ri][oidx] = occupent
+	nb.cost += (abs(src_hri-dst_hri) + idx + 2 + oidx) * cost[occupent]
+	return nb, true
+}
+
+func (b *burrow) hallToRoom(idx int) (nb burrow, ok bool) {
+	occupent := b.hall[idx]
+	dst_ri := roomIdx[occupent]
+	src_hri := hallwayRoomIdxs[dst_ri]
+	if !b.hallClear(idx, src_hri) {
+		return
+	}
+	oidx, open := b.roomOpen(dst_ri)
+	if !open {
+		return
+	}
+	nb = b.clone()
+	nb.hall[idx] = 0
+	nb.rooms[dst_ri][oidx] = occupent
+	nb.cost += (abs(idx-src_hri) + 1 + oidx) * cost[occupent]
+	return nb, true
 }
 
 // b.hall[from] is expected to be occupied
-func (b burrow) hallClear(from, to int) bool {
+func (b *burrow) hallClear(from, to int) bool {
 	if from < to {
-		for i := from+1; i <= to; i++ {
+		for i := from + 1; i <= to; i++ {
 			if b.hall[i] != 0 {
 				return false
 			}
 		}
 		return true
 	}
-	for i := from-1; i >= to; i-- {
+	for i := from - 1; i >= to; i-- {
 		if b.hall[i] != 0 {
 			return false
 		}
@@ -208,7 +187,7 @@ func abs(x int) int {
 	return int(math.Abs(float64(x)))
 }
 
-func part1(b burrow) int {
+func minCostArrangment(b burrow) int {
 	costs = []int{}
 	arrange(b)
 	min := int(1e10)
@@ -221,12 +200,37 @@ func part1(b burrow) int {
 }
 
 func main() {
-	sample := burrow{}
-	sample.rooms = [4][2]rune{{'B', 'A'}, {'C', 'D'}, {'B', 'C'}, {'D', 'A'}}
-	input := burrow{}
-	input.rooms = [4][2]rune{{'B', 'D'}, {'A', 'C'}, {'A', 'B'}, {'D', 'C'}}
+	sample1 := burrow{}
+	sample1.rooms = [4][]rune{{'B', 'A'}, {'C', 'D'}, {'B', 'C'}, {'D', 'A'}}
+	input1 := burrow{}
+	input1.rooms = [4][]rune{{'B', 'D'}, {'A', 'C'}, {'A', 'B'}, {'D', 'C'}}
+
+	sample2 := burrow{}
+	sample2.rooms = [4][]rune{
+		{'B', 'D', 'D', 'A'},
+		{'C', 'C', 'B', 'D'},
+		{'B', 'B', 'A', 'C'},
+		{'D', 'A', 'C', 'A'},
+	}
+	input2 := burrow{}
+	input2.rooms = [4][]rune{
+		{'B', 'D', 'D', 'D'},
+		{'A', 'C', 'B', 'C'},
+		{'A', 'B', 'A', 'B'},
+		{'D', 'A', 'C', 'C'},
+	}
 
 	fmt.Println("Part One")
-	fmt.Printf("\tsample: %d\n", part1(sample))
-	fmt.Printf("\t input: %d\n", part1(input))
+	fmt.Printf("\tsample: %d\n", minCostArrangment(sample1))
+	fmt.Printf("\t input: %d\n", minCostArrangment(input1))
+	fmt.Println("Part Two")
+	fmt.Printf("\tsample: %d\n", minCostArrangment(sample2))
+	fmt.Printf("\t input: %d\n", minCostArrangment(input2))
+
+	// Part One
+	//	 sample: 12521
+	//	  input: 15237
+	// Part Two
+	//	 sample: 44169
+	//	  input: 47509
 }
