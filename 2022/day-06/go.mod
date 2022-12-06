3b120919816987d8e2e2ddf8a8cc7d7f8f15d2d0
jw <53386281+jwnet@users.noreply.github.com>
2022-12-06 01:07:38 -0600

    day-06 complete

---
+++ 2022/day-06/go.mod
@@ -0,0 +1,7 @@
+module github.com/jwnet/advent-of-code/2022/day-06
+
+go 1.19
+
+replace github.com/jwnet/useful => /home/jw/code/go/src/github.com/jwnet/useful
+
+require github.com/jwnet/useful v0.0.0-00010101000000-000000000000
---
+++ 2022/day-06/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jwnet/useful/list"
+)
+
+func findEndOfMarker(mesg string, markerLen int) int {
+	buffer := list.List[rune]{}
+	rMap := make(map[rune]int8)
+
+	for i, r := range input {
+		buffer.Push(r)
+		rMap[r] += 1
+		if buffer.Len() == markerLen {
+			if len(rMap) == markerLen {
+				return i + 1
+			}
+			_, end := buffer.Drop()
+			if n := rMap[end]; n > 1 {
+				rMap[end] = n - 1
+			} else {
+				delete(rMap, end)
+			}
+		}
+	}
+	panic("you must have bad input!")
+}
+
+// part 1: 1109
+// part 2: 3965
+func main() {
+	fmt.Printf("part 1: %d\n", findEndOfMarker(input, 4))
+	fmt.Printf("part 2: %d\n", findEndOfMarker(input, 14))
+}
